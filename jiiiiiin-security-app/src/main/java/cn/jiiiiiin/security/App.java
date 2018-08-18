30e8591289ccfa53bd9c9bf56df9423ef16abef5
zhaojin <jiiiiiin@yeah.net>
2018-08-18 23:59:51 +0800

    feat:创建项目结构，引入依赖 https://coding.imooc.com/lesson/134.html#mid=6837

---
+++ jiiiiiin-security-app/src/main/java/cn/jiiiiiin/security/App.java
@@ -0,0 +1,13 @@
+package cn.jiiiiiin.security;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
