6a9b1971952aaf548ddbb80a443b36c473dd10fa
zhaojin <jiiiiiin@yeah.net>
2018-08-21 09:20:38 +0800

    feat: 学习springboot处理异常

--- README.md
+++ README.md
@@ -1,8 +1,34 @@
 - [代码结构](https://ws2.sinaimg.cn/large/006tNbRwgy1fue02z4h20j31kw0rc0y8.jpg)
 - [RESTFul API](https://ws3.sinaimg.cn/large/006tNbRwgy1fufeoc5gxdj31kw0yswnl.jpg)
 
+### 关键点
+    + springboot 默认错误处理控制器 `org.springframework.boot.autoconfigure.web.BasicErrorController`
+    
+        ```java
+        
+        // /error是默认错误视图的路径
+        @Controller
+        @RequestMapping("${server.error.path:${error.path:/error}}")
+        public class BasicErrorController extends AbstractErrorController {
+        
+            // 表示请求参数头中的Accept被认定为html的时候进入该接口进行错误处理
+            @RequestMapping(produces = "text/html")
+            
+            // 否则进入：
+            @RequestMapping
+            @ResponseBody
+            public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
+        ```
+        
+        手动处理404网页版：需要在`/jiiiiiin-security/jiiiiiin-security-demo/src/main/resources/resources/error/404.html`创建对应状态码的页面；
+        这样对应状态码的网页版的错误页面就可以被订制；
+            
 
 ### 问题集合：
+
+    **需要先创建对应数据库，启动redis服务**
+    
+    + [redis安装](https://www.youtube.com/watch?v=JGvbEk4jtrU)
 
     + `Cannot determine embedded database driver class for database type NONE`：
     
--- jiiiiiin-security-demo/src/main/java/cn/jiiiiiin/security/web/controller/UserController.java
+++ jiiiiiin-security-demo/src/main/java/cn/jiiiiiin/security/web/controller/UserController.java
@@ -2,6 +2,7 @@ package cn.jiiiiiin.security.web.controller;
 
 import cn.jiiiiiin.security.dto.User;
 import cn.jiiiiiin.security.dto.UserQryCondition;
+import cn.jiiiiiin.security.exception.UserNotExistException;
 import com.fasterxml.jackson.annotation.JsonView;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
@@ -20,54 +21,54 @@ import java.util.List;
 
 /**
  * @author jiiiiiin
+ * // @RestController 表明需要将当前类声明为一个RESTful的api提供者
+ * // @RequestMapping 作用是将http请求的url映射到java的方法上去
+ * // @RequestParam 表示需要将http请求参数映射成java方法的参数
+ * // public List<User> query(@RequestParam String username) 标识请求需要携带一个参数（GET）
+ * // @RequestParam(name="username") String name 标识传递的参数的key和方法的参数不统一的时候，通过name或者value来进行映射
+ * // @PathVariable 作用是将http请求的url参数映射到java的方法对应参数上去
+ * //@GetMapping("/user/{id}")
+ * //public User getUserInfo(@PathVariable Long id, @PathVariable(name = "id") Long idddd) {
+ * // 使用正则表达式对 path variable进行校验@GetMapping("/user/{id:\\d+}")
+ * <p>
+ * // @RequestBody 映射请求体到java方法参数
+ * // public User create(@RequestBody User user) 需要加上注解之后，才能解析前端传上来的json对象中的对应字段的值
+ * // spring mvc直接将前端传递的参数映射到一个实体类：public List<User> query(UserQryCondition condition){
+ * <p>
+ * // @Valid 注解和BindingResult验证请求参数的合法性并处理校验结果
+ * // public User create(@Valid @RequestBody User user) 如果直接这样写，那么前端如果没有按要求传递，则会直接不进入请求方法体，直接响应400 标识请求的格式错误，因为后台会校验对应字段的格式，但是校验失败
+ * // 如果需要在校验失败也进入方法体，需要添加BindingResult到方法参数中
+ * // BindingResult需要配置@Valid注解使用，在校验失败时候，会将错误消息（校验失败消息）映射到该对象
+ * <p>
+ * // @PageableDefault 用来指定分页参数的默认值
+ * // @PageableDefault(page = 1, size = 10, sort = "username,asc") Pageable pageable
+ * // 如果前端没有传递PageableDefault设置的默认值，那么就使用默认值映射到Pageable参数对应字段中
+ * <p>
+ * // @JsonView 控制接口的json输出内容
+ * // 使用接口来声明多个视图（比如查询用户列表、和查询单用户对象）
+ * // 如
+ * //public interface UserSimpleView {
+ * //}
+ * //
+ * 继承 {@link User.UserSimpleView} 就能把上层接口定义的东西继承下来
+ * <p>
+ * //public interface UserDetailView extends User.UserSimpleView {
+ * //}
+ * // 在值对象的get方法上指定视图
+ * // 如：
+ * //@JsonView(User.UserSimpleView.class)
+ * //public String getUsername() {
+ * //        return username;
+ * //        }
+ * //
+ * //@JsonView(User.UserDetailView.class)
+ * //public String getPassword() {
+ * //        return password;
+ * //        }
+ * <p>
+ * // 在Controller方法上指定视图
+ * // @JsonView(User.UserSimpleView.class)
  */
-// @RestController 表明需要将当前类声明为一个RESTful的api提供者
-// @RequestMapping 作用是将http请求的url映射到java的方法上去
-// @RequestParam 表示需要将http请求参数映射成java方法的参数
-// public List<User> query(@RequestParam String username) 标识请求需要携带一个参数（GET）
-// @RequestParam(name="username") String name 标识传递的参数的key和方法的参数不统一的时候，通过name或者value来进行映射
-// @PathVariable 作用是将http请求的url参数映射到java的方法对应参数上去
-//@GetMapping("/user/{id}")
-//public User getUserInfo(@PathVariable Long id, @PathVariable(name = "id") Long idddd) {
-// 使用正则表达式对 path variable进行校验@GetMapping("/user/{id:\\d+}")
-
-// @RequestBody 映射请求体到java方法参数
-// public User create(@RequestBody User user) 需要加上注解之后，才能解析前端传上来的json对象中的对应字段的值
-// spring mvc直接将前端传递的参数映射到一个实体类：public List<User> query(UserQryCondition condition){
-
-// @Valid 注解和BindingResult验证请求参数的合法性并处理校验结果
-// public User create(@Valid @RequestBody User user) 如果直接这样写，那么前端如果没有按要求传递，则会直接不进入请求方法体，直接响应400 标识请求的格式错误，因为后台会校验对应字段的格式，但是校验失败
-// 如果需要在校验失败也进入方法体，需要添加BindingResult到方法参数中
-// BindingResult需要配置@Valid注解使用，在校验失败时候，会将错误消息（校验失败消息）映射到该对象
-
-// @PageableDefault 用来指定分页参数的默认值
-// @PageableDefault(page = 1, size = 10, sort = "username,asc") Pageable pageable
-// 如果前端没有传递PageableDefault设置的默认值，那么就使用默认值映射到Pageable参数对应字段中
-
-// @JsonView 控制接口的json输出内容
-// 使用接口来声明多个视图（比如查询用户列表、和查询单用户对象）
-// 如
-//public interface UserSimpleView {
-//}
-//
-///**
-// * 继承 {@link User.UserSimpleView} 就能把上层接口定义的东西继承下来
-// */
-//public interface UserDetailView extends User.UserSimpleView {
-//}
-// 在值对象的get方法上指定视图
-// 如：
-//@JsonView(User.UserSimpleView.class)
-//public String getUsername() {
-//        return username;
-//        }
-//
-//@JsonView(User.UserDetailView.class)
-//public String getPassword() {
-//        return password;
-//        }
-
-// 在Controller方法上指定视图
 
 @RestController
 @RequestMapping("/user")
@@ -99,7 +100,7 @@ public class UserController {
     }
 
     @DeleteMapping("/{id:\\d+}")
-    public void delete(@PathVariable String id){
+    public void delete(@PathVariable String id) {
         L.info("delete id {}", id);
     }
 
@@ -117,10 +118,11 @@ public class UserController {
 
     @GetMapping("/{id:\\d+}")
     @JsonView(User.UserDetailView.class)
-    public User getUserInfo(@PathVariable Long id, @PathVariable(name = "id") Long idddd) {
+    public User getUserInfo(@PathVariable String id, @PathVariable(name = "id") Long idddd) {
         L.info("getUserInfo qry id is {}", id);
-        final User res = new User();
-        res.setUsername("tom");
-        return res;
+        throw new UserNotExistException(id);
+//        final User res = new User();
+//        res.setUsername("tom");
+//        return res;
     }
 }
