7fd02b616503feac4a9e02948adfd9fde4473f49
zhaojin <jiiiiiin@yeah.net>
2018-11-29 16:45:28 +0800

    refactor: 导出测试数据库文件`./db/vplusdb_2018-11-29.sql` ;

--- db/data-mysql.sql
+++
@@ -1,4 +0,0 @@
-TRUNCATE TABLE mng_admin;
-
-INSERT INTO mng_admin (username, password, phone, email)
-VALUES ('admin', '$2a$10$XQi3SDI8aU8VL8PQkkyddOYk62OmDBtLwD9f9EEKf0AZBI0Y7pwPq', '15399999999', '15399999999@163.com');
